97f0f7edb486d7225398316c682c8f4d63145a95
oles <olchyk89@gmail.com>
2018-11-11 00:32:29 +0100

    Added styles

--- app-lo/src/App.js
+++ app-lo/src/App.js
@@ -12,10 +12,10 @@ class App extends Component {
   render() {
     return (
       <div className="box">
-        <h1 className="box-counter"></h1>
+        <h1 className="box-counter">2</h1>
         <div className="box-controls">
-          <Button _onClick={ ()=>null }>+</Button>
           <Button _onClick={ ()=>null }>-</Button>
+          <Button _onClick={ ()=>null }>+</Button>
         </div>
       </div>
     );
